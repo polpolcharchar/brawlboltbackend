55787751012631d42b88329fc3ad3818e0af7e81
polpolcharchar <jackfitterer876@gmail.com>
2025-07-06 12:27:34 -0700

    no longer store map overrides in database, update utility

--- DatabaseUtility/modeToMapOverrideUtility.py
+++ DatabaseUtility/modeToMapOverrideUtility.py
@@ -1,36 +1,33 @@
 import json
 import boto3
 
-_mapToModeOverrides = None
+_mapToModeOverrides = {
+    "Moonbark Meadow": "dodgeBrawl",
+    "Rebound Ring": "dodgeBrawl",
+    "Hug or Hurl": "dodgeBrawl",
+    "Side Hustle": "dodgeBrawl",
+    "Squish Court": "dodgeBrawl",
+    "Wispwillow Ward": "dodgeBrawl",
+
+    "Arena of Glory": "brawlArena",
+    "Mirage Arena": "brawlArena",
+    "Knockout Grounds": "brawlArena",
+    "The Smackdome": "brawlArena",
+
+    "Super Center": "brawlHockey",
+    "Slippery Slap": "brawlHockey",
+    "Bouncy Bowl": "brawlHockey",
+    "Below Zero": "brawlHockey",
+    "Cool Box": "brawlHockey",
+    "Starr Garden": "brawlHockey",
+
+    "Snowcone Square": "brawlHockey5V5",
+    "Massive Meltdown": "brawlHockey5V5",
+    "Frostbite Rink": "brawlHockey5V5",
+    "Cold Snap": "brawlHockey5V5"
+}
+
 def getMode(game):
-    def fetchAndAssignOverrides():
-        global _mapToModeOverrides
-
-        DYNAMODB_REGION = 'us-west-1'
-        MAP_TO_MODE_OVERRIDES_TABLE = "BrawlStarsMapToModeOverrides"
-        dynamodb = boto3.client("dynamodb", region_name=DYNAMODB_REGION)
-
-        try:
-            response = dynamodb.get_item(
-                TableName=MAP_TO_MODE_OVERRIDES_TABLE,
-                Key={'overrideType': {'S': 'standard'}}
-            )
-
-            if 'Item' in response and 'overrides' in response['Item']:
-                overrides_raw = response['Item']['overrides']['S']
-                overrides = json.loads(overrides_raw)
-                _mapToModeOverrides = overrides
-            else:
-                print("Overrides not found.")
-                _mapToModeOverrides = {}
-        except Exception as e:
-            print(f"An error occurred: {e}")
-            _mapToModeOverrides = {}
-
-    if _mapToModeOverrides is None:
-        print("Fetching")
-        fetchAndAssignOverrides()
-
     if 'map' in game['event'] and game['event']['map'] in _mapToModeOverrides:
         return _mapToModeOverrides[game['event']['map']]
     elif 'mode' in game['event'] and game['event']['mode'] != "unknown":
