1f1deaf7ee770bd3f54c581379eddaf14830dc7b
polpolcharchar <jackfitterer876@gmail.com>
2025-07-30 22:24:13 -0700

    Switch back to using secrets in .env. For lambda, secrets are stored through the function's environment variables and accessed in the same way

--- DatabaseUtility/secretsUtility.py
+++ DatabaseUtility/secretsUtility.py
@@ -1,31 +1,14 @@
-import json
-import boto3
+import os
+from dotenv import load_dotenv
 
-_secretsMap = None
 def getSecret(secretName):
-    def fetchAndAssignSecrets():
-        global _secretsMap
+    value = os.environ.get(secretName)
+    if value is not None:
+        return value
 
-        DYNAMODB_REGION = 'us-west-1'
-        SECRETS_TABLE = "BrawlStarsSecrets"
-        dynamodb = boto3.client("dynamodb", region_name=DYNAMODB_REGION)
-
-        try:
-            response = dynamodb.get_item(
-                TableName=SECRETS_TABLE,
-                Key={'id': {'S': 'allSecrets'}}
-            )
-
-            if 'Item' in response and 'jsonValues' in response['Item']:
-                secretsJsonString = response['Item']['jsonValues']['S']
-                parsedSecrets = json.loads(secretsJsonString)
-                _secretsMap = parsedSecrets
-            else:
-                _secretsMap = {}
-        except Exception as e:
-            _secretsMap = {}
+    load_dotenv()
+    value = os.environ.get(secretName)
+    if value is not None:
+        return value
     
-    if _secretsMap is None:
-        fetchAndAssignSecrets()
-    
-    return _secretsMap[secretName]
+    raise KeyError(f"Secret '{secretName}' not found in environment or .env file")
